26e8f8e4980d1c0e33a9cdf2974cf7b7d1544054
Adam Devoe <adam.devoe@sematree.com>
2024-01-05 11:25:09 -0500

    First blood

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Sparky m = new Sparky();
+        m.go();
+        System.out.println("Done.");
+        System.out.println("Foofoo");
+    }
+}
---
+++ src/main/java/org/example/Sparky.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Sparky {
+    public void go() {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome Donkey Thong!");
+
+        for (int i = 1; i <= 10; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            String yy = "dykjstra";
+            System.out.println("i =  " + yy + " " + i);
+
+        }
+        System.out.println("done");
+    }
+}
